e4d9a3709021109978bb96f7124650dec38143df
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-09-19 22:23:13 -0400

    buttons are alterting on click - having issues showing gifs on click.  Will come back to this tomorrow afternoon

--- assets/javaScript/giphy.js
+++ assets/javaScript/giphy.js
@@ -13,120 +13,143 @@ for (var i = 0; i < topics.length; i++) {
     topicButtons.addClass("topic-buttons");
     topicButtons.attr("data-buttons", elements);
     topicButtons.text(elements);
+    // $(topicButtons).attr("value", topics[i]);
 
     console.log(topicButtons);
     $("#buttons-for-topics").append(topicButtons);
 }
+$("#buttons-for-topics").on("click", function(){ 
+    alert("TESTING");
 
-var topic = "snowboarding";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+    // work in progress comeback tomorrow 
+    event.preventDefault();
+    
+    var topic = $(this).attr("data-buttons");
+    queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response){
+        // console.log(response);
+        var results = response.data;
+        for (var i = 0; i < topics.length; i++) {
+            // const element = topics[i];
+
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
+                $("#show-gifs");
+            }
+            
+        }
+        // put code to connect gifs to buttons here 
+    });
 });
+// var topic = "space";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "space";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "dogs";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "dogs";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "harry potter";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "harry potter";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "x-ray";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "x-ray";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "food";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "food";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "1980's";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "1980's";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "groot";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "groot";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// var topic = "new york";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-var topic = "new york";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+//     // put code to connect gifs to buttons here 
+// });
 
-var topic = "travel";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+// var topic = "travel";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-    // put code to connect gifs to buttons here 
-});
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response){
+//     console.log(response);
+//     // put code to connect gifs to buttons here 
+// });
 
 
 
