47b6f7bc566af509ee10de5de3aa590662db6ddf
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-09-19 21:21:33 -0400

    buttons are styled and API's are logging

--- assets/javaScript/giphy.js
+++ assets/javaScript/giphy.js
@@ -2,7 +2,7 @@
 
 // giphy key - S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK
 
-var topics = ["Snowboarding", "Space", "Dogs", "Harry Potter", "X-ray", "Food", "1980's", "Groot", "New York"];
+var topics = ["Snowboarding", "Space", "Dogs", "Harry Potter", "X-ray", "Food", "1980's", "Groot", "New York", "Travel"];
     // console.log(topics);
 
 for (var i = 0; i < topics.length; i++) {
@@ -17,6 +17,129 @@ for (var i = 0; i < topics.length; i++) {
     console.log(topicButtons);
     $("#buttons-for-topics").append(topicButtons);
 }
+
+var topic = "snowboarding";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "space";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "dogs";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "harry potter";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "x-ray";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "food";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "1980's";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "groot";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "new york";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+var topic = "travel";
+queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK";
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+    // put code to connect gifs to buttons here 
+});
+
+
+
+
+
+
+                    // pseudocode
+// add API to code using Ajax syntax
+// make onclick event for topic buttons to call related gifs
+//     - add ratings
+// make form to add additional favorite topics (with submit button)
+// make gifs pausable
+
 
 
 
