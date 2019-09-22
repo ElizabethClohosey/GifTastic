c9b63011431fdabb16609236129b94c3d5ed47a3
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-09-22 19:44:08 -0400

    fully functioning

--- assets/javaScript/giphy.js
+++ assets/javaScript/giphy.js
@@ -1,6 +1,3 @@
-// alert("testing");
-
-// giphy key - S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK
 
 var topics = ["Snowboarding", "Space", "Dogs", "Harry Potter", "X-ray", "Food", "Toadstool", "Groot", "New York", "Archer", "South Park"];
     console.log(topics);
@@ -8,7 +5,6 @@ var topics = ["Snowboarding", "Space", "Dogs", "Harry Potter", "X-ray", "Food", 
 // function to create buttons for topics 
 function renderButtons () {
     for (var i = 0; i < topics.length; i++) {
-        // console.log("LOOP FOR TOPICS " + element);
         var topicButtons = $("<button>");
         topicButtons.addClass("topic-buttons");
         topicButtons.text(topics[i]);
@@ -21,29 +17,21 @@ renderButtons();
 
 $(document).ready(function(){ 
     // function to add user topic of choice from input to DOM and topics array 
-    $("#user-addition").on("click", function(event) {
+$("#user-addition").on("click", function(event) {
         event.preventDefault();
         var newTopic = $("#user-topic").val().trim();
         topics.push(newTopic);
-        
-        console.log("Input Topic " + newTopic);
-        // var inputTopic = topics[topics.length - 1];
         $("#buttons-for-topics").empty();
         renderButtons();
         $("#user-topic").val('');
-        // newTopic.addClass("topic-buttons");             
-        // newTopit.html(inputTopic);
-        // $("#buttons-for-topics").append(inputTopic);
     });
 
     // function to add gifs when each topic button is clicked 
-    $(".topic-buttons").on("click", function(){ 
-        // alert("TESTING");
+    $(document).on("click", ".topic-buttons", function(){ 
         event.preventDefault();
-        
-        var topic = $(this).val();
 
-        queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK&limit=10";
+        var topic = $(this).val();
+        queryURL = "https://api.giphy.com/v1/gifs/search?q="  + topic + "&api_key=S0zplPv4eNUbSVpvs05l7cC3Fuwo2OPK&limit=10&rating=g";
 
         $.ajax({
             url: queryURL,
@@ -52,9 +40,6 @@ $(document).ready(function(){
             // console.log(response);
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
-                // const element = topics[i];
-
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
                     var gifDiv = $("<div>");
                     var rating = results[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
@@ -67,13 +52,7 @@ $(document).ready(function(){
                     gifDiv.append(p);
                     gifDiv.append(gifImage);
                     $("#show-gifs").prepend(gifDiv);
-                }
             }
-        });
-
-        
-
-        
         });
     });
 
@@ -86,13 +65,5 @@ $(document).ready(function(){
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
             }
-
+    });
 });
-
-//                     fix 
-// buttons are not working onclick after adding user topic
-// user topic button is not loading gifs onclick
-// limit is different 
-
-                    // solved
-// gifs after first set of gifs are not animating
